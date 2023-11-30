2dd4f78639678006692848931a3c5435513d5542
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-30 12:40:14 +0200

    add build

--- src/components/utility/wrong-answer.ts
+++
@@ -1,9 +0,0 @@
-export function wrongAnswer() {
-    const content = document.querySelector('.content');
-    if (content) {
-        content.classList.add('wrong');
-        setTimeout(() => {
-            content.classList.remove('wrong');
-        }, 400);
-    }
-}
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import './assets/_nullstyle.scss';
-import './global.scss';
-import { App } from './components/app/app';
-
-new App();
